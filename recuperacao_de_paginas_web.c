f4e6d42b67cdfc0b9f3867a1a55c521e3949e735
gabepk <usuario@localhost.localdomain>
2016-11-21 14:29:27 -0200

    Recebendo msg, porem 302

--- recuperacao_de_paginas_web.c
+++ recuperacao_de_paginas_web.c
@@ -8,20 +8,22 @@
 #include <netinet/in.h>
 #include <netdb.h> /*gethostbyname()*/
 
+#define PORT 80
+
 char email[1024];
 char msg[1024];
-char *host;
+char host[1024];
 
 void connectToSocket ()
 {
   int sockfd, is_connected;
-  struct sockaddr_in serv_addr;
+  struct sockaddr_in server_addr;
   struct hostent *server;
 
   sockfd = socket(AF_INET, SOCK_STREAM, 0);
   if ( sockfd < 0)
   {
-    printf("Nao foi possivel obter descritor do socket.\n");
+    printf("Erro: Nao foi possivel obter descritor do socket.\n");
     exit(1);
   }
   
@@ -31,32 +33,23 @@ void connectToSocket ()
     exit(1);
   }
 
-  //memset(&server_addr, 0 , sizeof(server_addr));
-  //server_addr.sin_family = AF_INET;
-  //server_addr.sin_port = htons(80);
-  //memcpy(&server_addr.sin_addr.s_addr, server->h_addr, server->h_length);
-  //server_addr.sin_addr.s_addr = inet_addr(HOST);
-  //inet_aton(host, (struct in_addr *)&server_addr.sin_addr.s_addr);
-  //memset(&(server_addr.sin_zero), '\0', 8);
-  /*
-  is_connected = connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
-  if ( is_connected < 0 ) {
-    printf("Nao foi possivel conectar com o host %s na porta %d", host, PORT);
-    printf("\nERRNO: %s\n", strerror(errno));
-    exit(1);
-  }*/
+  memset(&server_addr, 0, sizeof(server_addr));
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(PORT);
+  memcpy(&server_addr.sin_addr.s_addr, server->h_addr, server->h_length);
+  if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+    printf("Erro: Nao foi possivel se conectar no Host %s, na porta %d", host, PORT);
+  }
 
+  int bytes_sent = send(sockfd, msg, strlen(msg), 0);
+  printf("bytes_sent: %d\n", bytes_sent);
+  printf("msg_sent: %s\n", msg);
+  
+  char resp[4096];
+  int bytes_recv = recv(sockfd, resp, sizeof(resp), 0);
+  printf("\n\nmsg_recv: %s\n", resp);
 
-  memset(&serv_addr,0,sizeof(serv_addr));
-  serv_addr.sin_family = AF_INET;
-  serv_addr.sin_port = htons(80);
-  memcpy(&serv_addr.sin_addr.s_addr,server->h_addr,server->h_length);
-  /* connect the socket */
-  if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0)
-    error("ERROR connecting");
-
-
-  else {printf("OK\n");}
+  return;
 }
 
 void setMsg (char *path) {
@@ -72,6 +65,7 @@ void setMsg (char *path) {
 int main (int argc, char *argv[]) {
   FILE *fp;
   
+
   /* TODO Verificar entrada "./r 1 -s"*/
   if (argc < 3) { 
     printf("Informe <URL> <nome do arquivo> <-s para sobrescrever arquivo>\n");
@@ -93,11 +87,10 @@ int main (int argc, char *argv[]) {
   }
   
   setMsg(argv[1]); /* TODO tirar host*, deixar soh path*/
-  printf("\n%s", msg);
   
   //strcpy(host, argv[1]);
   
-  host = "www.google.com";
+  strncpy(host, "www.google.com", 16);
 
   connectToSocket();
   
