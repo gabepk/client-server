4ef3839f9f05c0dc2f50854175295de416b01f2a
Gabriella de Oliveira Esteves <gabriella@localhost.localdomain>
2016-11-22 12:40:09 -0500

    Lendo a msg a partir de offsets diferentes

--- recuperacao_de_paginas_web.c
+++ recuperacao_de_paginas_web.c
@@ -1,13 +1,13 @@
 /*$Id: $*/
 #include <stdio.h>
 #include <stdlib.h>
-#include <regex.h>
 #include <string.h>
 #include <errno.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <unistd.h>
 
 #define PORT 80
 
@@ -54,29 +54,24 @@ void connect_to_socket ()
   int offset = 0;
   int i, j=0, total;
   char content_length[4];
-  char buffer[1024];
+  char buffer[200];
  
-  bytes_sent = write(sockfd, msg, strlen(msg));
+  bytes_sent = send(sockfd, msg, strlen(msg), 0);
   printf("Bytes sent: %d\n", bytes_sent);
-  printf("Message sent: \n%s\n", msg);
+  printf("Message sent: \n%s", msg);
   
-  printf("Message received: \n");
+  printf(".\nMessage received: \n");
   memset(buffer, 0, sizeof(buffer));
-/*
-  bytes_recv = read(sockfd, buffer, sizeof(buffer));
-  if (bytes_recv > 0) {
-    if ((i = strstr(buffer, "Content-Length: ")) != NULL) {
-      while (buffer[i+16] != '\r') {
-        content_length[j] = buffer[i+16];
-	i++;j++;
-      }
-    }
-  }*/
-  
-  
+ 
+  bytes_recv = recv(sockfd, buffer, sizeof(buffer), 0);
+  printf("%s <\n\n\n", buffer); 
+
+  bytes_recv = recv(sockfd, buffer+100, sizeof(buffer), 0);
+  printf("%s <", buffer+100); 
 
   do {
-    bytes_recv = read(sockfd, buffer + offset, strlen(buffer));
+    bytes_recv = recv(sockfd, buffer, sizeof(buffer), 0);
+    printf("$s <", buffer);
     if (bytes_recv < 0) {
       printf("Erro ao ler mensagem do socket.\n");
     }
@@ -96,17 +91,8 @@ void connect_to_socket ()
 void set_msg (char *path) {
   char length[4];
 
-  //strncpy(msg, "GET ", 5);
-  //strncat(msg, path, strlen(path));
-  //strncat(msg, " HTTP/1.0\r\n", 12);
-  
-  snprintf(msg, 16+strlen(path), "GET %s HTTP/1.0\r\n", path);
-  
-  //strncat(msg, "Content-Type: text/html\r\n", 26);
-  //strncat(msg, "Content-Length: ", 17);
-  //sprintf(lengthed "%d", strlen(msg) + 4);
-  //strncat(msg, length, 4);
-  //strncat(msg, "\r\n", 3);
+  snprintf(msg, 18+strlen(path), "GET %s HTTP/1.0\r\n\r\n", path);
+  //snprintf(msg, 40, "GET %s HTTP/1.0\r\n\r\n", path);
   return;
 }
 
@@ -133,26 +119,9 @@ int main (int argc, char *argv[]) {
     fp = fopen(argv[2], "a+");
   }
   
-  /* Parser da URL usando Regex */
-  /*
-  regex_t reg;
-  int is_regex_valid;
-  char url_pattern[40];
-  
-  strncpy(url_pattern, "^(([^:/?#]+):)?(//([^/?#]*))?([^?#]*)", 40);
-  is_regex_valid = regcomp(&reg, url_pattern, REG_EXTENDED|REG_NOSUB); 
-  
-  if (is_regex_valid == 0) {
-    if ((&reg != NULL) && (regexec(&reg, argv[1], (size_t) 0, (regmatch_t *) NULL, 0) == 0)) {
-      printf("host ok\n");
-     // regfree(&reg);
-    }
-  }
-  */
-
-  set_msg(argv[1]);
-  strncat(host, argv[1], strlen(argv[1]));
-
+  //set_msg(argv[1]);
+  strncat(host, "server.aker.com.br", 20);
+  set_msg("http://server.aker.com.br/intranet_aker/");
   connect_to_socket();
   
 
