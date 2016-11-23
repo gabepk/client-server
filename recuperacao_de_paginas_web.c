868bcd7fe3259058f270d8326fcd904c377a6588
Gabriella de Oliveira Esteves <gabriella@localhost.localdomain>
2016-11-23 12:52:04 -0500

    Identificando header. Escrevendo apenas conteudo no arquivo

--- recuperacao_de_paginas_web.c
+++ recuperacao_de_paginas_web.c
@@ -10,8 +10,10 @@
 #include <unistd.h>
 
 #define PORT 80
-#define BUFFER_SIZE 5
+#define BUFFER_SIZE 500
+#define HEADER_BUFFER_SIZE 1024
 #define MAX_STRING_SIZE 1024
+
 
 FILE *fp;
 int sockfd;
@@ -27,13 +29,13 @@ void connect_to_socket ()
   sockfd = socket(AF_INET, SOCK_STREAM, 0);
   if ( sockfd < 0)
   {
-    printf("Erro: Nao foi possivel obter descritor do socket.\n");
+    perror("\nError trying to get socket");
     exit(1);
   }
 
   server = gethostbyname(host);
   if (!server) {
-    printf("Erro: Host %s nao existe.\n", host);
+    herror("\nError trying to get server from host");
     exit(1);
   }
 
@@ -43,34 +45,79 @@ void connect_to_socket ()
   memcpy(&server_addr.sin_addr.s_addr, server->h_addr, server->h_length);
   if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) 
       < 0) {
-    printf("Erro: Nao foi possivel se conectar no Host %s, na porta %d.\n", 
-           host, PORT);
+    perror("\nError trying to connect to server");
   }
   
   return;
 }
 
 void send_message(int sockfd) {
-  int bytes_sent;
+  int bytes_sent, offset, msg_size;
 
-  bytes_sent = send(sockfd, msg, strlen(msg), 0);
-  if (bytes_sent < 0) {
-    printf("Erro ao enviar mensagem para o socket.\n");
-    exit(1);
-  }
-  printf("\n[Message sent] \t%s", msg);
-  
+  msg_size = strlen(msg);
+  offset = 0;
+  printf("\n[Message sent] \t");
+
+  memset(buffer, 0, BUFFER_SIZE);
+  do {
+    strncpy(buffer, msg + offset, BUFFER_SIZE);
+    bytes_sent = send(sockfd, buffer, BUFFER_SIZE, 0);
+    if (bytes_sent < 0) {
+      perror("\nError trying to send message from server");
+      exit(1);
+    }
+    else if (bytes_sent == 0) {
+      break;
+    }
+    offset += BUFFER_SIZE;
+    printf("%s", buffer);
+  } while (offset < msg_size);
+ 
   return;
 }
 
 void recv_message(int sockfd) {
   int bytes_recv;
 
+  /* Header */
+ 
+  int i = 0, still_header = 1;
+  char header[HEADER_BUFFER_SIZE], *end_of_header;
+ 
+  memset(header, 0, HEADER_BUFFER_SIZE);
+  do {
+    memset(buffer, 0, BUFFER_SIZE);                                             
+    bytes_recv = recv(sockfd, buffer, BUFFER_SIZE, 0);                          
+    if (bytes_recv < 0) {                                                       
+      perror("\nError trying to receive message from server");                  
+      exit(1);                                                                  
+    }                                                                           
+    else if (bytes_recv == 0) {                                                 
+      break;                                                                    
+    }
+
+    strncat(header, buffer, BUFFER_SIZE);    
+    end_of_header = strstr(header, "\r\n\r\n");
+
+    if (end_of_header) {
+      int pos = end_of_header - header;
+
+      for (i = pos+4; i < BUFFER_SIZE; i++)
+        fprintf(fp, "%c", header[i]);
+    
+      still_header = 0; 
+    }
+    //printf("header[%d] : %s\n", i, header);
+
+  } while (still_header == 1);
+
+  /* Content */
+
   do {
     memset(buffer, 0, BUFFER_SIZE);
     bytes_recv = recv(sockfd, buffer, BUFFER_SIZE, 0);
     if (bytes_recv < 0) {
-      printf("Erro ao receber mensagem do socket.\n");
+      perror("\nError trying to receive message from server");
       exit(1);
     }
     else if (bytes_recv == 0) {
@@ -83,10 +130,8 @@ void recv_message(int sockfd) {
 }
   
 int main (int argc, char *argv[]) {
-  
-  /* TODO Verificar entrada "./r 1 -s"*/
   if (argc < 3) { 
-    printf("Informe <URL> <nome do arquivo> <-s para sobrescrever arquivo>\n");
+    printf("\nEnter <URL> <file name> <-s to overwrite file>\n\n");
     exit(1);
   }
   
@@ -94,24 +139,28 @@ int main (int argc, char *argv[]) {
   /* Arquivo ja existe */
   if (fp) {
     /* Se flag de sobrescrita nao foi informada, exit. */
-    if (argv[3] && strncmp(argv[3], "-s", 2) != 0) {
-      printf("Arquivo ja existe e nao pode ser sobrescrito.\n");
-      printf("Para sobreescreve-lo, adicione nos parametros a flag -s.\n");
+    if (!argv[3] || (argv[3] && strncmp(argv[3], "-s", 2) != 0)) {
+      printf("\nFile ''%s'' already exists.\n", argv[2]);
+      printf("To overwrite it, add the flag ''-s'' to the end of the args\
+             list.\n\n");
       exit(1);
     }
   }
   else {
     fp = fopen(argv[2], "a+");
+    if (!fp) {
+      perror("\nError opening the file");
+      exit(1);
+    }
   }
-  
-  strncat(host, "server.aker.com.br", 20);
-  snprintf(msg, 58, "GET server.aker.com.br HTTP/1.0\r\n\r\n");
-  //snprintf(msg, 18+strlen(path), "GET %s HTTP/1.0\r\n\r\n", path);
+ 
+  strncat(host, argv[1], strlen(argv[1]));
+  snprintf(msg, 19+strlen(host), "GET %s/ HTTP/1.0\r\n\r\n", host);
 
   connect_to_socket();
-  send_message(sockfd);
+  send_message(sockfd); 
   recv_message(sockfd);
-  close(sockfd);  
+  close(sockfd);
   fclose(fp);
   return 0;
 }
